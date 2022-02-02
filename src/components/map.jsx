656027169f40d8794a99d3c2d410fb3e7604d4bb
Doni-gg1 <sapar.u.daniel@gmail.com>
2022-02-02 15:07:54 +0600

    added start animation

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,8 +1,23 @@
-import React, {useCallback, useRef, useState} from "react";
-import { GoogleMap, useLoadScript, Marker, InfoWindow, } from "@react-google-maps/api";
-import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
-import {Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
+import React, { useCallback, useRef, useState } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 import "@reach/combobox/styles.css";
+import { Spring, animated } from 'react-spring'
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -11,7 +26,7 @@ const mapContainerStyle = {
 };
 
 const option = {
-    disableDefaultUI: false,
+  disableDefaultUI: false,
 };
 
 export default function Map({ getWeather, lat, lng, setLat, setLng }) {
@@ -25,34 +40,54 @@ export default function Map({ getWeather, lat, lng, setLat, setLng }) {
   };
 
   const searchBtn = () => {
-      document.querySelector('.search').classList.toggle('notActiveInput')
-      document.querySelector('.search-icon').classList.toggle('activeSearch-icon')
-      document.querySelector('.search input').focus()
-  }
+    document.querySelector(".search").classList.toggle("notActiveInput");
+    document
+      .querySelector(".search-icon")
+      .classList.toggle("activeSearch-icon");
+    document.querySelector(".search input").focus();
+  };
 
   const mapRef = useRef();
   const onMapLoad = React.useCallback((map) => {
-      mapRef.current = map;
-  }, [])
+    mapRef.current = map;
+  }, []);
 
-  const panTo = useCallback(({lat,lng}) => {
-    mapRef.current.panTo({lat, lng});
-    mapRef.current.setZoom(14)
-  }, [])
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
 
-    const [markers, setMarkers] = useState({})
+  const [markers, setMarkers] = useState({});
 
-    if (loadError) return "Error";
+  if (loadError) return "Error";
   if (!isLoaded) return "Loading...";
 
-
   return (
-    <div>
-      <Search setMarkers={setMarkers} panTo={panTo} getWeather={getWeather} setLat={setLat} setLng={setLng} />
-      <button style={{transition: '0.5s'}} className="searchBtn search-icon" onClick={searchBtn}>
+    <div style={{borderRadius: "50%"}}>
+      <Search
+        setMarkers={setMarkers}
+        panTo={panTo}
+        getWeather={getWeather}
+        setLat={setLat}
+        setLng={setLng}
+      />
+      
+      <Spring
 
-      </button>
-      <GoogleMap
+        from={{opacity: "1", transform: "scale(100)"}}
+        to={{opacity: "1", transform: "scale(1)"}}
+         config={{duration: 500}} 
+      >
+        {
+          styles => (
+
+        <animated.div style={styles}>
+          <button
+        style={{ transition: "0.5s" }}
+        className="searchBtn search-icon"
+        onClick={searchBtn}
+        ></button>
+        <GoogleMap
           mapContainerStyle={mapContainerStyle}
           zoom={3}
           center={center}
@@ -61,17 +96,25 @@ export default function Map({ getWeather, lat, lng, setLat, setLng }) {
             setLat(event.latLng.lat());
             setLng(event.latLng.lng());
             getWeather(event.latLng.lat(), event.latLng.lng());
-            setMarkers({lat: event.latLng.lat(), lng: event.latLng.lng()})
-        }}
+            setMarkers({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+          }}
           onLoad={onMapLoad}
-      >
-          <Marker position={markers} onClick={ (e) => {
-            // document.querySelector('.wrapper_weatherItem').setAttribute("hidden", false)
-              document.querySelector('.weatherItem').classList.toggle('notActive')
-
-          } }
+        >
+          <Marker
+            position={markers}
+            onClick={(e) => {
+              // document.querySelector('.wrapper_weatherItem').setAttribute("hidden", false)
+              document
+                .querySelector(".weatherItem")
+                .classList.toggle("notActive");
+            }}
           />
-      </GoogleMap>
+        </GoogleMap>
+
+        </animated.div>
+          )
+        }
+      </Spring>
     </div>
   );
 }
@@ -93,27 +136,26 @@ function Search({ getWeather, setLat, setLng, panTo, setMarkers }) {
     },
   });
 
-
-    return (
+  return (
     <div className="search notActiveInput">
       <Combobox
         onSelect={async (address) => {
-          setValue('')
+          setValue("");
           try {
             const result = await getGeocode({ address });
             const { lat, lng } = await getLatLng(result[0]);
             setLat(lat);
             setLng(lng);
             getWeather(lat, lng);
-            panTo({lat, lng});
-            setMarkers({ lat, lng})
+            panTo({ lat, lng });
+            setMarkers({ lat, lng });
           } catch (error) {
             console.log(error);
           }
         }}
       >
         <ComboboxInput
-            style={{transition: "0.5s"}}
+          style={{ transition: "0.5s" }}
           value={value}
           onChange={(e) => {
             setValue(e.target.value);
